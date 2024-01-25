60dd3562060388eb93355cbcb931f34bf637f7fc
AliHassan <malihassanahmad@gmail.com>
2024-01-25 12:44:52 -0800

    defined stack class structure

--- Stack.cpp
+++ Stack.cpp
@@ -5,15 +5,62 @@ struct Node {
     Node* link;
     int data;
 
-    // Default constructor
-    Node() : link(nullptr), data(0) {}
+//Default Constructor
+    Node():link(nullptr),data(0){}
+//Parametrized Constructor
+    Node(int data):link(nullptr),data(data){}
+};
+class Stack
+{
+private:
+    Node* stackTop;
+    int stackSize;
+    // member functions -Empty Body
+    void pushStack(int x)
+    {
 
-    // Parameterized constructor
-    Node(int data) : link(nullptr), data(data) {}
+    }
+    int popStack()
+    {
+        return 1;
+    }
+    bool isEmpty() const
+    {
+        if (this->stackTop == nullptr)
+            return true;
+        return false;
+    }
+    void destroyStack()
+    {
+        
+    }
+public:
+    Stack()
+    {
+        this->stackTop=nullptr;
+        this->stackSize=0;
+    }
+    ~Stack()
+    {
 
-    // Destructor
-    ~Node() {
-        // Free any memory allocated by this node, if any
-        delete link;
+    }
+    //user member functions -Empty Body
+    void push(int x)
+    {
+
+    }
+    int pop()
+    {
+        return 1;
+    }
+    int getSize()
+    {
+        return this->stackSize;
+    }
+    int getStackTop()
+    {
+        return -1;
     }
 };
+
+
